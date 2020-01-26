ba082af17cefff84898fcdecfa4ca5929029e4f7
Adam Nielsen <malvineous@shikadi.net>
2020-01-26 23:30:54 +1000

    Add left-padding output function

--- cli/output.js
+++ cli/output.js
@@ -3,5 +3,11 @@ function pad(field, width, fnColour) {
 	return fnColour(s) + ''.padEnd(width - s.length);
 }
 
+function padLeft(field, width, fnColour) {
+	const s = '' + field;
+	return ''.padEnd(width - s.length)+ fnColour(s);
+}
+
 module.exports = console.log;
 module.exports.pad = pad;
+module.exports.padLeft = padLeft;
